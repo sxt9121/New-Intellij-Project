293ede569eb2410682fe833dcd43995c1f939a7d
sajina <sajina1tiwari@gmail.com>
2022-09-30 17:26:40 -0500

    new commit add class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the roll no");
+        int rollno=sc.nextInt();
     }
 }
